8664c8cdf13c62328012dd3fda4d8e5e6b5a306c
Junya Kaneko <jyuneko@gmail.com>
2015-12-02 21:40:13 +0900

    Added licenses and copyrights.

--- chapter3/problem2.py
+++ chapter3/problem2.py
@@ -1,6 +1,29 @@
+# This file is part of "Junya's self learning project about Neural Network."
+#
+# "Junya's self learning project about Neural Network"
+# is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "Junya's self learning project about Neural Network"
+# is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# (c) Junya Kaneko <jyuneko@hotmail.com>
+
+
 from chapter3.problem1 import sigmoid
 import numpy as np
 import logging
+
+
+__author__ = 'Junya Kaneko <jyuneko@hotmail.com>'
 
 
 class ProbabilisticElement:
