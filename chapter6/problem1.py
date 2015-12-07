2b5a5e11657fa7b7a90aea11d666221eefbc43c1
Junya Kaneko <jyuneko@gmail.com>
2015-12-07 18:10:13 +0900

    Added license.

--- chapter6/problem1.py
+++ chapter6/problem1.py
@@ -1,3 +1,23 @@
+# This file is part of "Junya's self learning project about Neural Network."
+#
+# "Junya's self learning project about Neural Network"
+# is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "Junya's self learning project about Neural Network"
+# is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# (c) Junya Kaneko <jyuneko@hotmail.com>
+
+
 import numpy as np
 from matplotlib import pyplot
 from time import time
