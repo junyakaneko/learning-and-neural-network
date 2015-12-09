d06ae3ccf6d063f8998e22e1f239bda7bb5c05a2
Junya Kaneko <jyuneko@gmail.com>
2015-12-09 13:18:57 +0900

    Changed its sequential learning algorithm.

--- chapter6/problem2.py
+++ chapter6/problem2.py
@@ -28,42 +28,57 @@ __author__ = 'Junya Kaneko <jyuneko@hotmail.com>'
 
 
 class Element:
-    """
-    どこか間違ってるんだと思うけど、付録にあるほど近似の精度がでない。
-    """
     def __init__(self, w, epsilon=0.5, nloops=100):
-        self._w = np.append([np.random.random(), ], w)
+        self._w = np.append([np.random.uniform(-5, 5), ], w)
         self._epsilon = epsilon
         self._nloops = nloops
 
     def get_value(self, x):
         return sigmoid(self._w.dot(np.append([1.0, ], x)))
 
-    def learn(self, datum):
+    def update(self, datum):
         x = datum[:-1]
         y = datum[-1]
-        for s in range(self._nloops):
-            val = self.get_value(x)
-            self._w -= self._epsilon * 2.0 * (val - y) * val * (1.0 - val) * np.append([1.0, ], x)
-        print(datum, np.abs(y - self.get_value(x)))
+        val = self.get_value(x)
+        self._w -= self._epsilon * 2.0 * (val - y) * val * (1.0 - val) * np.append([1.0, ], x)
 
+    def learn(self, data):
+        for i in range(self._nloops):
+            for datum in data:
+                self.update(datum)
 
 if __name__ == '__main__':
     data = np.array([[0.0, 1.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 3.0, 1.0], [2.0, 1.0, 1.0], [1.5, 2.0, 1.0]])
 
-    element = Element([-2.0, 3.0])
-    for datum in data:
-        element.learn(datum)
+    elements = [
+        Element([np.random.uniform(-5, 5), np.random.uniform(-5, 5)], nloops=10),
+        Element([np.random.uniform(-5, 5), np.random.uniform(-5, 5)], nloops=100),
+        Element([np.random.uniform(-5, 5), np.random.uniform(-5, 5)], nloops=1000),
+    ]
 
-    fig = pyplot.figure()
-    ax = fig.add_subplot(211, projection='3d')
+    for element in elements:
+        element.learn(data)
 
     x0s, x1s, ys = np.hsplit(data, 3)
 
-    ax.scatter(x0s, x1s, ys)
-    ax.plot(x0s, x1s, [element.get_value(datum[:-1]) for datum in data])
+    fig = pyplot.figure()
+    ax = fig.add_subplot(211, projection='3d')
+    ax.scatter(x0s, x1s, ys, label='Actual')
+    for element in elements:
+        ax.plot(x0s, x1s, [element.get_value(datum[:-1]) for datum in data], label='%s' % element._nloops)
+    pyplot.xlabel('x')
+    pyplot.ylabel('y')
+    pyplot.title('Actual data and approximations')
+    pyplot.legend(loc="upper left")
 
     pyplot.subplot(212)
-    pyplot.bar([i for i in range(len(data))], [np.abs(datum[-1] - element.get_value(datum[:-1])) for datum in data], align='center', width=0.4)
+    for i, element in enumerate(elements):
+        pyplot.bar([i, ],
+                   np.sum([np.power(datum[-1] - element.get_value(datum[:-1]), 2) for datum in data]),
+                   align='center', width=0.4)
+    pyplot.xticks(range(len(elements)), [element._nloops for element in elements])
+    pyplot.xlabel('Number of learning loops')
+    pyplot.ylabel('Square root error')
+    pyplot.title('Square root errors')
 
     pyplot.show()
